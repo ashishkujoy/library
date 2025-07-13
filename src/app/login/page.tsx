fa0085b122eda4aa594c8302bb83dd1fa042d521
Ashish Kumar <ashishkujoy@gmail.com>
2025-07-13 13:18:43 +0530

    Adds login screen

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import Login from "../../../components/Login";
+
+export default function LoginPage() {
+    return (
+        <div style={{ paddingBottom: '80px' }}>
+            <Login />
+        </div>
+    )
+}
