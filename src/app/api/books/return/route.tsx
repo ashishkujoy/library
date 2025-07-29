8127e5631ad9d08c4563128e4d2af2a3143afb3c
Ashish Kumar <ashishkujoy@gmail.com>
2025-07-29 17:48:05 +0530

    Adds return book route

---
+++ src/app/api/books/return/route.tsx
@@ -0,0 +1,34 @@
+import { returnBook } from "../../../../../db/borrowedBooks";
+import { getLoggedInUser } from "../../../../../db/user";
+
+export const POST = async (req: Request) => {
+    const { barcode } = await req.json();
+    const user = await getLoggedInUser();
+    if (!user) {
+        return new Response(JSON.stringify({ message: "User not logged in" }), {
+            status: 401,
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+    }
+
+    try {
+        // Assuming returnBook is a function that handles the logic of returning a book
+        await returnBook(user.id, barcode);
+        return new Response(JSON.stringify({ message: "Returned book successfully" }), {
+            status: 200,
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+    } catch (error) {
+        console.error("Error returning book:", error);
+        return new Response(JSON.stringify({ message: "Failed to return book" }), {
+            status: 500,
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+    }
+}
