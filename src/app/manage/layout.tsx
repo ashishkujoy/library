adb4cd73c2ce1c0cf07d4271cb72a6949758df33
Ashish Kumar <ashishkujoy@gmail.com>
2025-08-13 13:29:24 +0530

    Adds admin check in management layout

--- src/app/manage/layout.tsx
+++ src/app/manage/layout.tsx
@@ -1,10 +1,18 @@
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../lib/auth';
 import './page.css';
+import Login from '@/components/Login';
 
 export default async function ManagementLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await getServerSession(authOptions);
+  const user = session?.user;
+  if (!user || !user.isAdmin) {
+    return <Login />
+  }
   return (
     <div>{children}</div>
   );
